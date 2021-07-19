c8e0c1f16d5498629ce11b5acf0cdb9f5554489b
Lucas de Araujo <araujolucas97@outlook.com>
2021-07-19 18:05:21 -0300

    add *

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://front-arena-deadlock.vercel.app"
+    origin: "*"
   }
 });
 
