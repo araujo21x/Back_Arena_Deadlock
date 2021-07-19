eb583fcd5bce2bf3b82a00ebad9301cb71c5e183
Lucas de Araujo <araujolucas97@outlook.com>
2021-07-19 17:55:00 -0300

    conexão com API v1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import server from './server.js';
-server.listen(3000, () => { console.log('listening on :3000')});
+import dotenv from 'dotenv';
+dotenv.config();
+
+server.listen(process.env.PORT, () => { console.log(`listening on :${process.env.PORT}`)});
